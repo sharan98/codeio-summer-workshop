dd801b2d2ca050d40ff253c1bc2f1a8966f62e5d
MPVishnu <vishnuprashanth14@gmail.com>
2019-06-19 12:19:37 +0530

    Cleaned up db.js

--- react-app/src/db.js
+++ react-app/src/db.js
@@ -12,17 +12,4 @@ mongoUtil.connectToServer( function( err, client ) {
   //mongoUtil.closeDB(); does not work yet
 } );
 
-// client.connect(err => {
-//   //const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   console.log("connected Successfully");
-//   const db = client.db('bmsce');
-//   console.log('created coll object');
-//   //console.log(db.collection('syllabus').count());
-//   db.collection('syllabus').find({}).toArray( function (err,result){
-//     if (err) throw err;
-//     console.log(result)
-//   });
-//   client.close();
-// });
 
